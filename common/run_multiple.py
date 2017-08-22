de0c0b035028b0193545c1f225a971893e813609
Bruno Del Papa <bdelpapa@gmail.com>
2017-08-22 14:00:17 +0200

    to cluster: L=1000, N=200

--- common/run_multiple.py
+++ common/run_multiple.py
@@ -14,7 +14,7 @@ from utils import backup_pickle
 # these values always overwrite the values in that file
 variables = ['A']
 values = [
-          np.arange(220, 1001, 20),
+          np.arange(1, 21, 1),
          ]
 # number of repetitions of each experiment (for statistics)
 total_runs = 2
