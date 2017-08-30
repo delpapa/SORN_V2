b29bef23d4cd936f7e3936a96a68134f0ed26b0d
Bruno Del Papa <bdelpapa@gmail.com>
2017-08-30 15:50:13 +0200

    to cluster: A = 20, N = 200; LearningCapacity

--- common/run_multiple.py
+++ common/run_multiple.py
@@ -14,7 +14,7 @@ from utils import backup_pickle
 # these values always overwrite the values in that file
 variables = ['L']
 values = [
-          np.array([4, 10, 20, 100, 1000, 2000, 5000]),
+          np.array([20, 40, 100, 1000, 2000, 5000]),
          ]
 # number of repetitions of each experiment (for statistics)
 total_runs = 1
