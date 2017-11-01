71ea94f3c5d7d72cdcfbed69644ebe816188a4db
Bruno Del Papa <bdelpapa@gmail.com>
2017-11-01 18:44:55 +0100

    A10

--- common/run_cluster.sh
+++ common/run_cluster.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=sleuths
 #SBATCH --ntasks=10
 #SBATCH --mem-per-cpu=7000
-#SBATCH --job-name="sorn_RandomSequenceTask_perfXtplast_A4"
-#SBATCH --output=sorn_RandomSequenceTask_perfXtplast_A4.out
+#SBATCH --job-name="sorn_RandomSequenceTask_perfXtplast_A10"
+#SBATCH --output=sorn_RandomSequenceTask_perfXtplast_A10.out
 #SBATCH --mail-user=delpapa@fias.uni-frankfurt.de
 #SBATCH --mail-type=END
 #SBATCH --time=7-00:00:00
